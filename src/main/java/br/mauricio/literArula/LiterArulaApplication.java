10cca2e57d1d050ca17fb7d8c5dd94b5e2a9df36
Maurício Dias <devxxx.mauricio@gmail.com>
2024-05-08 16:02:08 -0300

    Add files via upload

---
+++ src/main/java/br/mauricio/literArula/LiterArulaApplication.java
@@ -0,0 +1,13 @@
+package br.mauricio.literArula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterArulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterArulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mauricio/literArula/LiterArulaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mauricio.literArula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterArulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
